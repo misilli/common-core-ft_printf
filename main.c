3ebfb848e8bf3853883dc6f42fb5219c92d30443
muharrem midilli <muharremmidilli@mail.com>
2026-08-25 19:58:11 +0300

    %c ve %s flagları içibn length  hesaplama eklendi

--- main.c
+++ main.c
@@ -6,9 +6,13 @@
 /*   By: mumidill <mumidill@student.42istanbul.com. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/23 20:00:03 by mumidill          #+#    #+#             */
-/*   Updated: 2026/08/24 19:36:57 by mumidill         ###   ########.fr       */
+/*   Updated: 2026/08/25 19:57:44 by mumidill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
+
+
 
 #include <stdarg.h>
 #include <stddef.h>
@@ -16,14 +20,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void	ft_putchar(int c)
+int	ft_putchar(int c)
 {
 	write(1, &c, 1);
+	return(1);
 }
 
-size_t	ft_strlen(const char *s)
+int	ft_strlen(const char *s)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
 	while (s[i])
@@ -31,16 +36,19 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
-void	ft_putstr(char *s)
+int	ft_putstr(char *s)
 {
 	int	i;
+	int len;
 
+	len = 0;
 	i = 0;
 	while (s[i] != '\0')
 	{
-		ft_putchar(s[i]);
+		len += ft_putchar(s[i]);
 		i++;
 	}
+	return(len);
 }
 
 void	ft_putnbr(long n)
@@ -59,13 +67,14 @@ void	ft_putnbr(long n)
 		ft_putchar(n + '0');
 }
 
-void	ft_putnbr_base(long nbr, char *base)
+int	ft_putnbr_base(long nbr, char *base)
 {
-	size_t	len;
+	int	len;
+	
 
 	len = ft_strlen(base);
 	if (len < 2)
-		return ;
+		return (67);
 	if (nbr < 0)
 	{
 		ft_putchar('-');
@@ -84,16 +93,16 @@ void	adresyazmafonksiyonu(void *p)
 
 	adres = (unsigned long)p;
 	ft_putstr("0x");
-	ft_putnbr_base(adres, "0123456789abcdef");
+	ft_putnbr_base(adres, "0123456789abcdef");0x7ffeb70cdedf
 }
 
 // va arg ile burda ilerlet
-void	ft_print(va_list i, int a, char c)
+int	ft_print(va_list i, char c)
 {
 	if (c == 'c')
-		ft_putchar(va_arg(i, int));
+		return(ft_putchar(va_arg(i, int)));
 	else if (c == 's')
-		ft_putstr(va_arg(i, char *));
+		return(ft_putstr(va_arg(i, char *)));
 	else if (c == 'p')
 		adresyazmafonksiyonu(va_arg(i, void *));
 	else if (c == 'd' || c == 'i')
@@ -104,37 +113,41 @@ void	ft_print(va_list i, int a, char c)
 		ft_putnbr_base(va_arg(i, int), "0123456789abcdef");
 	else if (c == 'X')
 		ft_putnbr_base(va_arg(i, int), "0123456789ABCDEF");
-	/*else if (c == '%')
-		printf("%%", i);*/
+	else if (c == '%')
+		printf("%%", i);
+	return(0);
 }
 
 int	ft_printf(const char *str, ...)
 {
 	va_list	values;
 	int		i;
-	int		val;
-
+	int		len;
+	
+	len = 0;
+	i = 0;
 	va_start(values, str);
 	while (str[i])
 	{
-		if (str[i] == '%')
+		if (str[i] == '%' && str[i + 1] != '\0')
 		{
-			ft_print(values, 0, str[i + 1]);
+			len += ft_print(values, str[i + 1]);
 			i++;
 		}
 		else
-			ft_putchar(str[i]);
+
+			len += ft_putchar(str[i]);
 		i++;
 	}
-	return (0);
+	va_end(values);
+	return (len);
 }
 
 int	main(void)
 {
-	char a = '2';
-	char b[] = "abc";
-	unsigned int c = 3000000;
-	char *pa = &a;
-	//printf("%p", (void *)pa);
-	ft_printf("%p", (void *)pa);
+	char *p;
+
+	p =12;
+	
+	printf("!!%p" , (void *)p);
 }
